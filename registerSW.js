63cb0acf57622b33dad75ad5e893e84b45ddf5bb
Muhammad Haziq <mhaziq.ahamid@gmail.com>
2026-08-20 00:27:45 +0800

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/personal-wealth-management/sw.js', { scope: '/personal-wealth-management/' })})}
